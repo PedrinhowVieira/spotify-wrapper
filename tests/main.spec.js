d2daf062b0025ac95c1dcd0e75efc119333ce121
PedrinhowVieira <pedro.dmvieira@gmail.com>
2019-03-11 15:18:03 -0300

    added all search tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -12,6 +12,18 @@ sinonStubPromise(sinon);
 global.fetch = require('node-fetch');
 
 describe('Spotify Wrapper', () => {
+  let fetchedStub;
+  let promise;
+
+  beforeEach(() => {
+    fetchedStub = sinon.stub(global, 'fetch');
+    promise = fetchedStub.returnsPromise();
+  });
+
+  afterEach(() => {
+    fetchedStub.restore();
+  });
+
   describe('Smoke Tests', () => {
     it('should exist the search method', () => {
       expect(search).to.exist;
@@ -35,18 +47,6 @@ describe('Spotify Wrapper', () => {
   });
 
   describe('Generic Search', () => {
-    let fetchedStub;
-    let promise;
-
-    beforeEach(() => {
-      fetchedStub = sinon.stub(global, 'fetch');
-      promise = fetchedStub.returnsPromise();
-    });
-
-    afterEach(() => {
-      fetchedStub.restore();
-    });
-
     it('should call fetch function', () => {
       const artists = search();
       expect(fetchedStub).to.have.been.calledOnce;
@@ -75,6 +75,66 @@ describe('Spotify Wrapper', () => {
       const artists = search('Drake', 'artist');
 
       expect(artists.resolveValue).to.be.eql({ body: 'json' });
+    });
+  });
+
+  describe('searchArtists', () => {
+    it('should call fetch function', () => {
+      const artists = searchArtists('Drake');
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      const artist1 = searchArtists('Drake');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Drake&type=artist');
+
+      const artist2 = searchArtists('LinkinPark');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=LinkinPark&type=artist');
+    });
+  });
+
+  describe('searchAlbums', () => {
+    it('should call fetch funcion', () => {
+      const albums = searchAlbums('Scorpion');
+      expect(fetchedStub).to.have.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      const album1 = searchAlbums('Scorpion');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Scorpion&type=album');
+
+      const album2 = searchAlbums('MoreLife');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=MoreLife&type=album');
+    });
+  });
+
+  describe('searchTracks', () => {
+    it('should call fetch funcion', () => {
+      const tracks = searchAlbums('Scorpion');
+      expect(fetchedStub).to.have.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      const track1 = searchTracks('Scorpion');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Scorpion&type=track');
+
+      const track2 = searchTracks('MoreLife');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=MoreLife&type=track');
+    });
+  });
+
+  describe('searchPlaylists', () => {
+    it('should call fetch funcion', () => {
+      const playlists = searchAlbums('Scorpion');
+      expect(fetchedStub).to.have.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      const playlist1 = searchPlaylists('Scorpion');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Scorpion&type=playlist');
+
+      const playlist2 = searchPlaylists('MoreLife');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=MoreLife&type=playlist');
     });
   });
 });
